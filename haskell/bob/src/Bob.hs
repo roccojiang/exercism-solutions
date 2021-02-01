902fdfad11d20b9b0a01fa090bd6985a12d401dd
Rocco Jiang <rocco.jiang5@gmail.com>
2021-02-01 14:25:36 +0000

    Change functions into values to avoid duplicate evaluation

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -4,14 +4,14 @@ import Data.Char ( isAlpha, isSpace, isUpper )
 
 responseFor :: String -> String
 responseFor s
-  | allSpaces s                    = "Fine. Be that way!"
-  | allAlphasUpper s && isQuestion = "Calm down, I know what I'm doing!"
-  | allAlphasUpper s               = "Whoa, chill out!"
-  | isQuestion                     = "Sure."
-  | otherwise                      = "Whatever."
+  | allSpaces                = "Fine. Be that way!"
+  | isShouting && isQuestion = "Calm down, I know what I'm doing!"
+  | isShouting               = "Whoa, chill out!"
+  | isQuestion               = "Sure."
+  | otherwise                = "Whatever."
   where
-    allSpaces        = all isSpace
-    isQuestion       = last (filter (not . isSpace) s) == '?'
-    allUpper         = all isUpper . filter isAlpha
-    anyAlpha         = any isAlpha
-    allAlphasUpper s = allUpper s && anyAlpha s
+    allSpaces  = all isSpace s
+    isQuestion = last (filter (not . isSpace) s) == '?'
+    allUpper   = all isUpper . filter isAlpha
+    anyAlpha   = any isAlpha
+    isShouting = allUpper s && anyAlpha s
